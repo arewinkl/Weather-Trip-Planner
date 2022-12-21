62428ae3ed253ab235a2c3b6e2a0c9d84e2cdc72
Alec Rewinkel <alecrewinkel@gmail.com>
2022-12-20 18:36:30 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Weather Planner</h2>
       </header>
+      <body>
+        <p>Take some time to check the weather for your upcoming location!</p>
+        <input type="search"></input>
+        <button>Search</button>
+      </body>
     </div>
   );
 }
