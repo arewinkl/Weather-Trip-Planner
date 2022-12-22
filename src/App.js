c5aab94caa81d96cc3cea8d3cefdebbee574f687
Alec Rewinkel <alecrewinkel@gmail.com>
2022-12-22 07:14:24 -0700

    getting weather

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
 import React from "react";
+import axios from "axios";
+// import getAllWeather from "./services/UserServices";
 
 const API_KEY = process.env.REACT_APP_WEATHER_KEY;
 console.log(API_KEY);
 
+function getAllWeather() {
+  axios
+    .get(
+      "https://api.openweathermap.org/data/2.5/weather?q=Denver,Colorado&appid=5f06a771d25b62ba916f476b3f89f810"
+    )
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+getAllWeather();
 function App() {
   return (
     <div className="App">
