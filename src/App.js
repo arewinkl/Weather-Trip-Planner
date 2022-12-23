b7034d6f47052713c4fd27103d920b1075dd56f8
Alec Rewinkel <alecrewinkel@gmail.com>
2022-12-23 16:29:50 -0700

    linking

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import "./App.css";
 import React from "react";
 import axios from "axios";
 import NavBar from "./components/NavBar";
+import { Route } from "react-router-dom";
+import Current from "./components/CurrentWeather";
+import Weekly from "./components/WeeklyForecast";
+import Map from "./components/WeatherMap";
+
 // import getAllWeather from "./services/UserServices";
 
 const API_KEY = process.env.REACT_APP_WEATHER_KEY;
@@ -27,6 +32,20 @@ function App() {
       <header className="App-header">
         <h2>Weather Planner</h2>
       </header>
+      <main>
+        <div>
+          {/* <Route exact path="/"></Route> */}
+          <Route exact path="/current">
+            <Current />
+          </Route>
+          <Route exact path="/weekly">
+            <Weekly />
+          </Route>
+          <Route exact path="/map">
+            <Map />
+          </Route>
+        </div>
+      </main>
     </div>
   );
 }
