f310006aecc9e2dd1b0f8933a7a38b794c4ee596
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-12-24 15:25:52 -0700

    get

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -1,16 +1,22 @@
 import API_KEY from "../App";
 import axios from "axios";
-function getAllWeather() {
+import App from "../App";
+import {useEffect, useState} from 'react'
+function GetAllWeather() {
   const API_KEY = process.env.REACT_APP_WEATHER_KEY
+  // const [data, setData] = useState([])
+
   axios
     .get(
       `https://api.openweathermap.org/data/2.5/weather?q=Denver,Colorado&appid=${API_KEY}`
     )
     .then((res) => {
       console.log(res.data);
+      return res.data
     })
+    // .then((data) => setData(data))
     .catch((err) => {
       console.log(err);
     });
 }
-export default getAllWeather;
+export default GetAllWeather;
