de679f26fe1ecd1f9f2307975ecd4ce03c13f1fc
Alec Rewinkel <alecrewinkel@gmail.com>
2023-01-28 22:07:27 -0700

    week data highs and lows

--- src/components/WeeklyForecast.js
+++ src/components/WeeklyForecast.js
@@ -5,12 +5,17 @@ export default function WeeklyForecast({ weekly }) {
 
   const all_temps = weekly.data.list;
 
-  const monday_temps = [];
-
-  console.log(all_temps);
+  const day_one_temps = [];
+  const day_two_temps = [];
+  const day_three_temps = [];
+  const day_four_temps = [];
+  const day_five_temps = [];
+  const day_six_temps = [];
+  const day_seven_temps = [];
+  //   console.log(all_temps);
   const weeklyData = [];
-  const dayOneWeather = [];
-  for (let x = 0, dateTemp = []; x < all_temps.length; x++) {
+
+  for (let x = 0, week = []; x < all_temps.length; x++) {
     if (
       !weeklyData.includes(
         new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3)
@@ -19,53 +24,72 @@ export default function WeeklyForecast({ weekly }) {
       weeklyData.push(
         new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3)
       );
-
-      //TODO All thus dateTemp code can probably be deleted, it just shows day of week and temperature in an array.
-      dateTemp.push(
-        new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3),
-        all_temps[x].main.temp
-      );
-    } else {
-      dateTemp.push(
-        new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3),
-        all_temps[x].main.temp
+      week.push(
+        new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3)
       );
     }
-    // if (
-    //   new Date(all_temps[x + 1].dt * 1000).toDateString().substring(0, 3) ===
-    //   new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3)
-    // ) {
-    //   monday_temps.push(all_temps[x].main.temp);
-    // }
+
+    //TODO Below is the if statements that match the day from the week variable that is set in the for loop and pushes the weather out for each day. All data needs to be sorted and then printed to the page.
     if (
-      new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3) === "Mon"
+      new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3) ===
+      week[0]
     ) {
-      monday_temps.push(all_temps[x].main.temp);
+      day_one_temps.push(all_temps[x].main.temp);
     }
-
-    // cow.push(
-    //   new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3),
-    //   all_temps[x].main.temp
-    // );
-    // if (
-    //   new Date(all_temps[x].dt).toDateString().substring(0, 3) ===
-    //   new Date(all_temps[x + 1].dt * 1000).toDateString().substring(0, 3)
-    // ) {
-    //   dayOneWeather.push(
-    //     all_temps[x].main.temp,
-    //     new Date(all_temps[x].dt).toDateString().substring(0, 3)
-    //   );
-    // }
-
-    //   let cat = cow.sort();
-    //   dayOneWeather.push(cat);
-    //   console.log("this is a cat", cat);
-    //   if (!dayOneWeather.includes(cat[0])) dayOneWeather.push(cat[0]);
+    if (
+      new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3) ===
+      week[1]
+    ) {
+      day_two_temps.push(all_temps[x].main.temp);
+    }
+    if (
+      new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3) ===
+      week[2]
+    ) {
+      day_three_temps.push(all_temps[x].main.temp);
+    }
+    if (
+      new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3) ===
+      week[3]
+    ) {
+      day_four_temps.push(all_temps[x].main.temp);
+    }
+    if (
+      new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3) ===
+      week[4]
+    ) {
+      day_five_temps.push(all_temps[x].main.temp);
+    }
+    if (
+      new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3) ===
+      week[5]
+    ) {
+      day_six_temps.push(all_temps[x].main.temp);
+    }
+    if (
+      new Date(all_temps[x].dt * 1000).toDateString().substring(0, 3) ===
+      week[6]
+    ) {
+      day_seven_temps.push(all_temps[x].main.temp);
+    }
   }
-  //   console.log(dayOneWeather);
 
   console.log(weeklyData);
-  console.log(monday_temps.sort(), "Monday");
+  console.log(
+    day_one_temps.sort(),
+    "day 1",
+    day_two_temps.sort(),
+    "day 2",
+    day_three_temps.sort(),
+    "day 3",
+    day_four_temps.sort(),
+    "day 4",
+    day_six_temps.sort(),
+    "day 6",
+    day_seven_temps.sort(),
+    "day 7"
+  );
+
   return (
     <div>
       {/* display the highest and lowest for each day of the week */}
